9ec1164ba9a142a7439d0eccf4bfd046591a5c40
devrsi0n <devrsi0n@gmail.com>
2018-10-31 21:10:50 +0800

    refactor: using immer in reducer

--- src/pages/content/reducers/index.js
+++ src/pages/content/reducers/index.js
@@ -1,5 +1,5 @@
+import produce from 'immer';
 import { SET_CONFIGS } from '../types';
-import { mergeDeep } from '../helpers';
 
 const initState = {
   start: {
@@ -73,56 +73,47 @@ const getTimeOptions = (duration, time) => {
   };
 };
 
-const keepWidthHeightRate = (state, newWidth) => {
+const keepWidthHeightRate = (state, isNewWidth) => {
   const { width, height, videoWidth, videoHeight } = state;
   if (!videoWidth || !videoHeight) {
-    return state;
+    return;
   }
-  if (newWidth) {
-    return {
-      height: Math.round(width * (videoHeight / videoWidth)),
-    };
+  if (isNewWidth) {
+    state.height = Math.round(width * (videoHeight / videoWidth));
   }
-  return {
-    width: Math.round(height * (videoWidth / videoHeight)),
-  };
+  state.width = Math.round(height * (videoWidth / videoHeight));
 };
 
+// Bad reducer design
+// TODO: refactor
 const configs = (state = initState, action) => {
   const { type, data } = action;
 
-  switch (type) {
-    case SET_CONFIGS: {
-      const result = {};
-      mergeDeep(result, state, data);
-      const keys = Object.keys(data);
-      if (
-        keys.includes('start') ||
-        keys.includes('end') ||
-        keys.includes('duration')
-      ) {
-        mergeDeep(result, {
-          startOptions: getTimeOptions(result.duration, result.start),
-          endOptions: getTimeOptions(result.duration, result.end),
-        });
-      }
-      if (keys.includes('width')) {
-        mergeDeep(result, keepWidthHeightRate(result, true));
-      } else if (keys.includes('height')) {
-        mergeDeep(result, keepWidthHeightRate(result, false));
-      }
+  return produce(state, draft => {
+    const keys = Object.keys(data);
+    switch (type) {
+      case SET_CONFIGS: {
+        if (
+          keys.includes('start') ||
+          keys.includes('end') ||
+          keys.includes('duration')
+        ) {
+          draft.startOptions = getTimeOptions(draft.duration, draft.start);
+          draft.endOptions = getTimeOptions(draft.duration, draft.end);
+        }
+        if (keys.includes('width')) {
+          keepWidthHeightRate(draft, true);
+        } else if (keys.includes('height')) {
+          keepWidthHeightRate(draft, false);
+        }
 
-      if (keys.includes('videoWidth') || keys.includes('videoHeight')) {
-        mergeDeep(result, {
-          width: Math.round(data.videoWidth / 3),
-          height: Math.round(data.videoHeight / 3),
-        });
+        if (keys.includes('videoWidth') || keys.includes('videoHeight')) {
+          draft.width = Math.round(data.videoWidth / 3);
+          draft.height = Math.round(data.videoHeight / 3);
+        }
       }
-      return result;
     }
-    default:
-      return state;
-  }
+  });
 };
 
 export default configs;
